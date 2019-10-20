11a908d966fa03f7d4cb57b78966bf27c74d131a
asmundh <aasmund.haugse@gmail.com>
2019-10-20 16:36:34 +0200

    Now starts again. Also fix vulnerabilities

--- server/babel.config.js
+++ server/babel.config.js
@@ -1,22 +1,20 @@
-module.exports = function(api) {
-   const presets = [
-     [
-       "@babel/env",
-       {
-         targets: {
-           node: "current"
-         },
-         useBuiltIns: "usage"
-       }
-     ]
-   ];
-   const plugins = [
-      "@babel/plugin-proposal-class-properties"
-   ];
- 
-   api.cache(true);
-   return {
-     presets,
-     plugins
-   };
- };
+module.exports = function (api) {
+  const presets = [
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 'current',
+        },
+        useBuiltIns: 'usage',
+      },
+    ],
+  ];
+  const plugins = [];
+
+  api.cache(true);
+  return {
+    presets,
+    plugins,
+  };
+};
